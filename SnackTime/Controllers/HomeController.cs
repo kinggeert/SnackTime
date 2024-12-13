c5890d05aa33b804456e2efa3233f3518af7d915
Geert <geertbruls@outlook.com>
2024-12-13 10:25:50 +0100

    Added HomeViewModel

--- SnackTime/Controllers/HomeController.cs
+++ SnackTime/Controllers/HomeController.cs
@@ -1,21 +1,36 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SnackTime.Data;
 using SnackTime.Models;
+using SnackTime.ViewModels;
 
 namespace SnackTime.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly DatabaseContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DatabaseContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var homeViewModel = new HomeViewModel
+        {
+            Products = _context.Products.ToList(),
+            Categories = _context.ProductCategories.ToList(),
+            //Temp
+            Basket = new Basket
+            {
+                Products = new List<ProductCount>()
+            }
+        };
+        return View(homeViewModel);
     }
 
     public IActionResult Privacy()
---
+++ SnackTime/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SnackTime.Models;
+
+namespace SnackTime.ViewModels;
+
+public class HomeViewModel
+{
+    public IEnumerable<ProductCategory> Categories { get; set; }
+    public IEnumerable<Product> Products { get; set; }
+    public Basket Basket { get; set; }
+}
