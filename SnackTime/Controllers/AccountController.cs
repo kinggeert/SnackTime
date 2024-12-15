ab0888b54e14a8461c51fba5d22c8906a791d932
Geert <geertbruls@outlook.com>
2024-12-15 11:20:19 +0100

    Added logout functionality

--- SnackTime/Controllers/AccountController.cs
+++ SnackTime/Controllers/AccountController.cs
@@ -52,4 +52,10 @@ public class AccountController : Controller
 
         return View(model);
     }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Index", "Home");
+    }
 }
