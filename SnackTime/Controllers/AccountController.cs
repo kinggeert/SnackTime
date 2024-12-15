137d706f8f6a75aeef8e520707e5cfec7ab5ffdf
Geert <geertbruls@outlook.com>
2024-12-15 09:57:04 +0100

    Added accountcontroller

---
+++ SnackTime/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnackTime.Data;
+
+namespace SnackTime.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly DatabaseContext _context;
+
+    public AccountController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
