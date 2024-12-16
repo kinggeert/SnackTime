be74f80d858f51ee9b1db474086e65c813fcb1dc
Geert <geertbruls@outlook.com>
2024-12-16 02:18:17 +0100

    Remove Privacy

--- SnackTime/Controllers/HomeController.cs
+++ SnackTime/Controllers/HomeController.cs
@@ -133,15 +133,7 @@ public class HomeController : Controller
 
         return RedirectToAction("Index");
     }
-
-
-
-
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
