5194ed8d617839f5d3c5d623a1ab291d5a1c810d
Geert <geertbruls@outlook.com>
2024-12-16 01:09:44 +0100

    Add user role management

---
+++ SnackTime/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SnackTime.Data;
+using SnackTime.Models;
+using SnackTime.ViewModels;
+
+namespace SnackTime.Controllers;
+
+public class AdminController(DatabaseContext context) : Controller
+{
+    private readonly DatabaseContext _context = context;
+
+    [Authorize(Roles = "Admin")]
+    public IActionResult Users()
+    {
+        var viewModel = new EditUserViewModel
+        {
+            AvailableRoles = _context.Roles.ToList()
+        };
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> EditUser(EditUserViewModel model)
+    {
+        if (model.Email != null && model.SelectedRoleName != null)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == model.Email);
+            if (user == null) return RedirectToAction("Users");
+            var role = _context.Roles.FirstOrDefault(r => r.Name == model.SelectedRoleName);
+            if (role == null) return RedirectToAction("Users");
+            user.Role = role;
+            _context.Entry(user).State = EntityState.Modified;
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction("Users");
+    }
+}
---
+++ SnackTime/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SnackTime.Models;
+
+namespace SnackTime.ViewModels;
+
+public class EditUserViewModel
+{
+    public string? Email { get; set; }
+    public ICollection<Role> AvailableRoles { get; set; }
+    public string? SelectedRoleName { get; set; }
+}
